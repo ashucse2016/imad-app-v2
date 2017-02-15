a0e04d642470798c949db742a2fbc02ba641c88a
ashucse2016 <ashucse2016@gmail.com>
2017-02-15 22:19:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,16 +1,72 @@
-var express = require('express');
+    var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title:'Article one| Tomcat Gopal',
+    heading:'article one',
+    date:'sep 5,2016',
+    content: `<p>
+                if you are beginner and you want to learn programming.you must have some right approach to start learning.
+                first step,always try to learn from offical docs which is provided ,when you downloaded IDE,if you have any problem regarding setup and programming.you can search on google.<strong>stackoverflow</strong> is good resource to clear your all kind of doubts.you can also watch videos from youtube.But,i recommend you to follow only one resource.otherwise ,you will get confuse and irritate while learning.golden rule:whatever you learn,try to implement i.e do practical.programming is all about practice practice and practice.so more u do practice,more u learn.
+            </p>
+            <p>
+                c language:it is procedural based language in which whole program is divided in form of function,local variable ,global variable and so on.for making of large program,it is become more complicated and take lot of space .
+                therefore,c++ language(superset of c language)is created to remove all this problem by implementing OOPs concept.
+            </p>`
+};
+
+function createTemplate (data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmlTemplate = `
+<html>
+    <head>
+        
+    <title>
+        ${title} 
+    </title>
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>
+    <link href="/ui/style.css" rel="stylesheet" />
+        
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            
+        <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+           ${content} 
+        
+        </div>
+        </div>
+        
+    </body>
+
+</html>`
+;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-    
+//    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));  
 });
 
 app.get('/article-two',function(req,res){
