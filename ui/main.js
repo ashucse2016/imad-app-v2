a3cbcddc72850b162cf7d7af1db0409346873175
ashucse2016 <ashucse2016@gmail.com>
2017-02-16 19:15:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML= 'New value';
 //move the image
-var img=element.getElementById('img');
+var img=element.getElementById('madi');
 img.onclick=function(){
   img.style.marginleft='100px';   
 };
