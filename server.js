b5ccaa04aa25813f58ec47f45ca3fe809c8d157d
ashucse2016 <ashucse2016@gmail.com>
2017-02-24 20:10:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,6 +93,18 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
+var names=[];
+//app.get('/submit-name/:name',function(req,res)
+app.get('/submit-name',function(req,res)//URL: /submit-name?name=xxxxx
+{
+    //get the name from request object
+    //var name=req.params.name;//TODO 
+    var name=req.query.name;
+    names.push(name);//here name is string,how can we convert array of object into string.
+//JSON:javascript object notation-way of converting javascript object into string
+    res.send(JSON.stringify(names));
+    
+});
 app.get('/:articleName',function(req,res){
 //    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
   //articleNmae==article-one
@@ -124,18 +136,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names=[];
-//app.get('/submit-name/:name',function(req,res)
-app.get('/submit-name',function(req,res)//URL: /submit-name?name=xxxxx
-{
-    //get the name from request object
-    //var name=req.params.name;//TODO 
-    var name=req.query.name;
-    names.push(name);//here name is string,how can we convert array of object into string.
-//JSON:javascript object notation-way of converting javascript object into string
-    res.send(JSON.stringify(names));
-    
-});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
