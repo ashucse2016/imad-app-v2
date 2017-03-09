58752075bd065748b531ba70beea0fe005da3701
ashucse2016 <ashucse2016@gmail.com>
2017-03-10 02:17:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,5 @@
-//counter code
-var button=document.getElementById('counter'); 
 
-button.onclick=function()
-{//create a request object
-var request=new XMLHttpRequest();
-
-//capture the response and store in a variable
-request.onreadystatechange=function()
-{if(request.readyState === XMLHttpRequest.DONE)
-{//take some action
-if(request.status===200)
-{
-    var counter=request.responseText;//take response store in counter variable
-    var span=document.getElementById('count');
-span.innerHTML=counter.toString();    //take counter variable and put into spam.
-}
-}
-//Not done yet
-
-    
-};
-//make the request
-request.open('GET','http://ashucse2016.imad.hasura-app.io/counter',true);
-request.send(null);
-    
-};
-//submit name
+//submit username/password to login
 
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
@@ -38,17 +12,16 @@ request.onreadystatechange=function()
 {//take some action
 if(request.status===200)
 {
-   //capture a list of name and render it as a list
-//var names=['name1','name2','name3','name4'];
-var names=request.responseText;
-names=JSON.parse(names);
-var list='';
-for(var i=0;i<names.length;i++)
-{list += '<li>' + names[i] + '</li>';
-    }
-    
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list; 
+   
+    console.log("user logged in");
+    alert('logged in successfully');
+}
+else if(request.status===403)
+{alert('username/password is incorrect');
+}
+else if(request.status===500)
+{
+    alert('something went wrong on the server');
 }
 }
 //Not done yet
@@ -56,10 +29,13 @@ for(var i=0;i<names.length;i++)
     
 };
 //make the request
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
-request.open('GET','http://ashucse2016.imad.hasura-app.io/submit-name?name=' + name,true);
-request.send(null);
+var username=document.getElementById('username').value;
+var password=document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST','http://ashucse2016.imad.hasura-app.io/login',true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({username:username,password:password}));
 
 
 };
